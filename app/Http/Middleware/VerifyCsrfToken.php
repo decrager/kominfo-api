74402d9ad55d534588c9d3168ac0774ed39e193c
Aldo Brahmantyo <aldonugroho@gmail.com>
2022-02-20 01:52:43 +0700

    CSRF token ex

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,18 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        '/loginAPI'
+        '/loginAPI',
+        '/AgendaCrt','/AgendaUpd/{id}','AgendaDest/{id}',
+        '/AlbumCrt','/AlbumUpd/{id}','AlbumDest/{id}',
+        '/BannerCrt','/BannerUpd/{id}','BannerDest/{id}',
+        '/BeritaCrt','/BeritaUpd/{id}','BeritaDest/{id}',
+        '/GalerifotoCrt','/GalerifotoUpd/{id}','GalerifotoDest/{id}',
+        '/GalerivideoCrt','/GalerivideoUpd/{id}','GalerivideoDest/{id}',
+        '/HalstatisCrt','/HalstatisUpd/{id}','HalstatisDest/{id}',
+        '/HeaderslideCrt','/HeaderslideUpd/{id}','HeaderslideDest/{id}',
+        '/KatBeritaCrt','/KatBeritaUpd/{id}','KatBeritaDest/{id}',
+        '/Kat_halstatisCrt','/Kat_halstatisUpd/{id}','Kat_halstatisDest/{id}',
+        '/PenggunaCrt','/PenggunaUpd/{id}','PenggunaDest/{id}',
+        '/ProfilopdCrt','/ProfilopdUpd/{id}','ProfilopdDest/{id}',
     ];
 }
