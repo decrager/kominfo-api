6712a5284f3b6fd1fd2b26eabd6896c9faaf7465
Aldo Brahmantyo <aldonugroho@gmail.com>
2022-02-21 23:34:35 +0700

    Little Patch

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('REST API Web Kominfo Kota Bogor') }}
+            {{ __('REST API Website Kominfo Kota Bogor') }}
         </h2>
     </x-slot>
 
