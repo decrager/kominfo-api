78a0b7a3a6a42191468f6a0adac203e2ef602287
Aldo Brahmantyo <aldonugroho@gmail.com>
2022-03-13 01:30:21 +0700

    Email Verify Updated

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -2,7 +2,7 @@
 {{-- Header --}}
 @slot('header')
 @component('mail::header', ['url' => config('app.url')])
-{{ "REST API Kominfo Kota Bogor" }}
+{{ "Verifikasi Email Pengguna" }}
 @endcomponent
 @endslot
 
